a3e4d9203dcb7772d6eb8618f7d3dd4fb0e1fe4d
Bas <85746482+Basgamer999@users.noreply.github.com>
2024-01-29 21:22:01 +0100

    Added counting fails to a user. And fixed the editing bug

--- src/events/client/countingEdited.js
+++ src/events/client/countingEdited.js
@@ -9,7 +9,7 @@ module.exports = {
       table: "counting",
     });
     if (result[0].LastMessageID == message.id) {
-        message.channel.send(`<@${message.author.id}> deleted their message. Counter is still at ${result[0].counter-1}`);
+        message.channel.send(`<@${message.author.id}> edited their message. Counter is still at ${result[0].counter-1}`);
     }
   },
 };
